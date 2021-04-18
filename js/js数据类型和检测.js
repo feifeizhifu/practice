467b5baaf9bb061d79d3f8fb1b3af74dccbe4bdd
lihuimin <lihuimin@uniev.com>
2021-04-18 19:20:42 +0800

    parseIn

--- js/js数据类型和检测.js
+++ js/js数据类型和检测.js
@@ -19,5 +19,9 @@ js中的数据类型
 /*
 Object.is(NaN,NaN) =>true
 */
-//最大安全数 最小安全数，超过该数字计算的话就不太准确了，可以使用BigInt(value)toString可以还原 
-console.log(Number.MAX_SAFE_INTEGER,Number.MIN_SAFE_INTEGER)
+//最大安全数 最小安全数，超过该数字计算的话就不太准确了，可以使用BigInt(value)toString可以还原
+console.log(Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER);
+
+//parseInt([value]) value以0x开头，默认会以16进制转10进制
+//  parseInt(0xad)=>173
+//parseInt(0123)=>83  value以0开头（浏览器），默认会以8进制转10进制
