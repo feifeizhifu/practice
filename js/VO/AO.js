d3b27809f9d42351c79b5fcbc2c762cd8938089d
feifeizhifu <1583314919@qq.com>
2021-05-09 21:56:39 +0800

    xn

--- js/VO/AO.js
+++ js/VO/AO.js
@@ -1,14 +1,43 @@
-var a={n:1}
-var b=a
-a.x=a={c:2}
-console.log(a,b)//{ c: 2 } { n: 1, x: { c: 2 } }
+// var a={n:1}
+// var b=a
+// a.x=a={c:2}
+// console.log(a,b)//{ c: 2 } { n: 1, x: { c: 2 } }
 
-var x=[12,23]
-function fn(y){
-  y[0]=100
-  y=[100]
-  y[1]=200
-  console.log(y)//[100,200]
+// var x=[12,23]
+// function fn(y){
+//   y[0]=100
+//   y=[100]
+//   y[1]=200
+//   console.log(y)//[100,200]
+// }
+// fn(x)
+// console.log(x)//[100,23]
+
+//函数执行的上下文是它的作用域--只和在哪船舰的有关系，和在哪执行没有关系
+var i=0
+function A(){
+  var i=10
+  function x(){
+    console.log(i)
+  }
+  return x
 }
-fn(x)
-console.log(x)//[100,23]
+var y=A()
+y()//10
+function B(){
+  var i=3
+  y()
+}
+B()//10
+
+let x=5
+function fn(x){
+  return function(y){
+    console.log(y+ ++x)
+  }
+}
+let f=fn(6)
+f(7)
+fn(8)(9)
+f(10)
+console.log(x)
